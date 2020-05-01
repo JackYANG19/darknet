9eb6f74b5adcd98674e67842d9cf056f172c5a16
JackYANG19 <48786606+JackYANG19@users.noreply.github.com>
2020-05-01 11:19:25 +0800

    Add person counting function

--- darknet_video.py
+++ darknet_video.py
@@ -14,24 +14,46 @@ def convertBack(x, y, w, h):
     ymax = int(round(y + (h / 2)))
     return xmin, ymin, xmax, ymax
 
-
 def cvDrawBoxes(detections, img):
+# initilalize the person counter variable
+    pc=0
     for detection in detections:
-        x, y, w, h = detection[2][0],\
-            detection[2][1],\
-            detection[2][2],\
+        x, y, w, h = detection[2][0], 
+            detection[2][1],
+            detection[2][2],
             detection[2][3]
         xmin, ymin, xmax, ymax = convertBack(
             float(x), float(y), float(w), float(h))
         pt1 = (xmin, ymin)
         pt2 = (xmax, ymax)
+        # add +1 to our person counter if we found a object 'person'
+        if str(detection[0].decode()) == 'person':
+        pc+=1
         cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
-        cv2.putText(img,
-                    detection[0].decode() +
-                    " [" + str(round(detection[1] * 100, 2)) + "]",
-                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    [0, 255, 0], 2)
-    return img
+        cv2.putText(img, 
+                    detection[0].decode()+
+                    " [" + str(round(detection[1] * 100, 2)) + "]", 
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [0, 255, 0], 2)
+    # return image and person counter
+    return img, pc
+
+#def cvDrawBoxes(detections, img):
+#    for detection in detections:
+#        x, y, w, h = detection[2][0],\
+#            detection[2][1],\
+#            detection[2][2],\
+#            detection[2][3]
+#       xmin, ymin, xmax, ymax = convertBack(
+#           float(x), float(y), float(w), float(h))
+#        pt1 = (xmin, ymin)
+#        pt2 = (xmax, ymax)
+#        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+#       cv2.putText(img,
+#                    detection[0].decode() +
+#                    " [" + str(round(detection[1] * 100, 2)) + "]",
+#                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+#                    [0, 255, 0], 2)
+#    return img
 
 
 netMain = None
@@ -103,7 +125,9 @@ def YOLO():
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-        image = cvDrawBoxes(detections, frame_resized)
+        image, pc = cvDrawBoxes(detections, frame_resized)
+        # added cv2.putText to draw the counter on our image
+        cv2.putText(img, "Persons: "+str(pc), (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,125,255), 2)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         print(1/(time.time()-prev_time))
         cv2.imshow('Demo', image)
